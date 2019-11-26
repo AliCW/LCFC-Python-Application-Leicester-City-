9553ff71740cf86cf35adee59a42307cec53f7d9
AliCW <53982566+AliCW@users.noreply.github.com>
2019-11-26 13:36:46 +0000

    Update read_version_player_stats.py
    Fixing errors.

--- version/read_version_player_stats.py
+++ version/read_version_player_stats.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 raw_path = str(Path().absolute()) #<--takes the raw path
-data_path = str('data\\')
+data_path = str('\\data\\')
 #print(raw_path.strip('version') + data_path)
 
 read_file = open(raw_path.strip('version') + data_path + 'current_player_stats.py') #<--takes /version/ out of the path so it can find run.py
