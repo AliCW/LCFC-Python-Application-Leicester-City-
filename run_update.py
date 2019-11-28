589e7b991fb28f280ff8d7a04ef5fc0181aecc2d
AliCW <53982566+AliCW@users.noreply.github.com>
2019-11-28 11:19:12 +0000

    Update run_update.py
    Changes have been made to the mthod in which the external version number reference is obtained for the update process. Now, instead of reading the number from an update table, the update program now reads the version numbers directly from the files online (first line) and uses that for comparison. The same function is performed with the local files stored on the users end, in this manner, the version numbers have all been correctly embedded into the data files themselves! (#25) (#15)

--- run_update.py
+++ run_update.py
@@ -1,4 +1,4 @@
-#v0.03
+#v0.04
 import subprocess
 import html
 import os
@@ -21,31 +21,16 @@ version_path = str('\\version\\')
 old_path = str('\\old\\')
 double_dash = str('\\')
 
-def check_gecko():
-    obtain_gecko_path = subprocess.check_output(['python', 'get_gecko_path.py']).strip() #outputs into bytes
-    string_gecko_path = obtain_gecko_path.decode('utf-8') #decode bytes into string
-    obtain_gecko_pid = subprocess.check_output(['python', 'get_gecko_pid.py']) #outputs into bytes
-    string_gecko_pid = obtain_gecko_pid.decode('utf-8') #decode bytes into string
-    int_gecko_pid = int(string_gecko_pid) #convert string to integer - couldnt figure out a way to decode into an integer yet!
-    if not int_gecko_pid: #if this string is empty, print statement &  move on
-        print('No PID found for geckodriver')
-    if not string_gecko_path:  #if this string is empty, print statement &  move on
-        print('No path found for geckodriver') #because of the placement of this definition, these statements should never arise
-    if string_gecko_path == base_path:
-        os.kill(int_gecko_pid, SIGTERM)
-        print('Update process is over.')
-    else: print('Error in check_gecko, geckodriver.exe may still be running.')
-
 try:#<-----Needs to check the version number in order to update, or decide not to
     open_run_file_version = open(base_path + version_path + 'read_version_run.py')
     open_run_file_version.close()
     run_file_version = subprocess.check_output(['python', base_path + version_path + 'read_version_run.py'])
 except IOError:
-    print('No version file found for run.py, obtaining one.')
+    print('No read file found for run.py, obtaining one.')
     options = Options()
     options.headless = True
     browser = webdriver.Firefox(options=options)
-    browser.get('https://raw.githubusercontent.com/AliCW/LCFC-Python-Application-Leicester-City-/master/version/run_version.py')
+    browser.get('https://raw.githubusercontent.com/AliCW/LCFC-Python-Application-Leicester-City-/master/version/read_version_run.py')
     WebDriverWait(browser, 10)
     new_run_version_raw = browser.page_source
     new_run_version_parsed = html.unescape(new_run_version_raw)
@@ -55,22 +40,27 @@ except IOError:
     run_version_create.write(new_run_version_final)
     run_version_create.close()
     browser.close()
-    check_gecko()
     run_file_version = subprocess.check_output(['python', base_path + version_path + 'read_version_run.py'])
 
 def update_run_file():
+    if os.path.exists(base_path + old_path): #checks of the old folder exists - blank folder not hosted on github
+        print('Existing .old folder found')
+    if not os.path.exists(base_path + old_path): #creates old folder for .old files to be moved
+        print('Creating .old folder...')
+        os.makedirs(base_path + old_path) #specifies the full directory path
     print('Checking for latest run.py file.')
     options = Options()
     options.headless = True
     browser = webdriver.Firefox(options=options)
-    browser.get('https://raw.githubusercontent.com/AliCW/LCFC-Python-Application-Leicester-City-/master/update/update_table.txt')
+    browser.get('https://raw.githubusercontent.com/AliCW/LCFC-Python-Application-Leicester-City-/master/run.py')
     WebDriverWait(browser, 10)
-    update_list = browser.page_source
-    escape_list = html.unescape(update_list)
-    run_soup = BeautifulSoup(escape_list, features='html.parser')
-    run_update_string = run_soup.h2.string
-    print('Latest run.py version: ' + run_update_string)
-    if float(run_update_string) > float(run_file_version):
+    run_version_raw = browser.page_source
+    run_version_parsed = html.unescape(run_version_raw)
+    run_version_soup = BeautifulSoup(run_version_parsed, features='html.parser')
+    run_update_string = run_version_soup.pre.string
+    run_version_number = run_update_string[2:6]
+    print('Latest run.py version: ' + run_version_number)
+    if float(run_version_number) > float(run_file_version):
         run_update_query = input('\nThe run.py file is out of date.\nWould you like to update? Y / N\n').lower()
         if run_update_query == yes:
             os.replace(base_path + double_dash + 'run.py', base_path + old_path + 'run.old.py')
@@ -84,15 +74,12 @@ def update_run_file():
             run_create.write(latest_run_final)
             run_create.close()
             browser.close()
-            check_gecko()
             print('run.py has been updated.')
         if run_update_query == no:
             print('Very Well...')
             browser.close()
-            check_gecko()
-    if float(run_update_string) <= float(run_file_version):
+    if float(run_version_number) <= float(run_file_version):
         browser.close()
-        check_gecko()
         print('run.py is up to date.')
         sys.exit()
 
